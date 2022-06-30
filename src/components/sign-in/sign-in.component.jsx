dc1dfdd88975cd5340f5142cc09d8ba1381ee623
TarDasT-7 <amirtardast7shiraz@gmail.com>
2022-06-30 13:40:24 +0430

    Form Input added and styles

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import './sign-in.styles.scss';
+import FormInput from "../../components/form-input/form-input.component";
+
+class SignIn extends React.Component{
+
+    constructor(props){
+        super(props);
+
+        this.state={
+            'email':'' ,
+            'password':''
+        }
+    }
+
+
+    HandleSubmit = event =>{
+        event.preventDefault();
+
+        this.setState({email:'' , password:''});
+    };
+
+    HandleChange = event =>{
+
+        // const {value , name} = useState();
+        const {value , name} = event.target;
+        this.setState({[name]:value});
+
+    };
+
+    render(){
+        return(
+            <div className="sign-in">
+                <h2>I Already Have An Accont</h2>
+                <span>Sign In With Email And Password</span>
+
+                <form onSubmit={this.HandleSubmit}>
+                    <FormInput 
+                        type="email" name="email" label="Email"
+                        value={this.state.email} handleChange={this.HandleChange}
+                        required 
+                    />
+
+                    <FormInput 
+                        type="password" name="password" label="Password"
+                        value={this.state.password} handleChange={this.HandleChange}
+                        required 
+                    />
+
+                    <input type="submit" value="submit form" />
+
+                </form>
+
+            </div>
+        );
+    }
+
+}
+
+export default SignIn;
