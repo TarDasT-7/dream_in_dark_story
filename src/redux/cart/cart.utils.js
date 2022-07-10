75880871a9edccf815a9d520866268054e4726e0
TarDasT-7 <amirtardast7shiraz@gmail.com>
2022-07-10 18:00:22 +0430

    checkout page

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -15,3 +15,21 @@ export const addItemToCart = (cartItems, cartItemToAdd) =>{
     return [...cartItems , {...cartItemToAdd , quantity:1}];
 
 };
+
+export const removeItemFromCart = (cartItem , cartItemToRemove)=>{
+
+    const existingCartItem = cartItem.find(
+        cartItem => cartItem.id === cartItemToRemove.id
+    )
+
+    if(existingCartItem.quantity === 1){
+        return cartItem.filter(cartItem => cartItem.id !== cartItemToRemove.id);
+    }
+
+    return cartItem.map(
+        cartItem => 
+        cartItem.id === cartItemToRemove.id ? 
+        {...cartItem , quantity : cartItem.quantity - 1} : cartItem
+    )
+
+};
